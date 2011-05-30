14d1e55d393a4489dd4db6fe3dff50e94fbf90f2
Waqas <waqas@waqas-desktop.(none)>
2011-05-30 17:50:31 +0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110518050754) do
+ActiveRecord::Schema.define(:version => 20110530115009) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "login"
